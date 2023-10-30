ec21d8a2681a9cabda63cd900e98836f70a9d0af
leoecrepont <leo.ecrepont@mergify.com>
2023-10-30 09:24:18 +0100

    work with forked repo

--- resolver.sh
+++ resolver.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "--- START ---"
-echo "Repo is $REPO"
+echo "Repo is $HEAD_REPO"
 echo "Head branch is $HEAD_BRANCH"
 echo "Base branch is $BASE_BRANCH"
 echo "User is $USER"
@@ -14,43 +14,74 @@ git remote -v
 git status
 git branch --show-current
 
+# 1) BASE is checked out by the action checkout
+# - if HEAD_REPO (from input) != current head repo (current)
+#       -> add HEAD_REPO origin
+remote_url=$(git config --get remote.origin.url)
+echo "$remote_url"
 
-# install poetry
-curl -sSL https://install.python-poetry.org | python3 -
-
-if ! git ls-remote --heads origin | grep -wq "refs/heads/$BASE_BRANCH"; then
-  echo "base branch '$BASE_BRANCH' does not exist"
+if [ -n "$remote_url" ]; then
+  git_url=$(echo "$remote_url" | awk -F/ '{print $1 "//" $3}')
+  repo=$(echo "$remote_url" | awk -F/ '{print $4 "/" $5}' | sed 's/\.git$//')
+else
+  echo "Could not find remote url"
   exit 1
 fi
 
-current_branch=$(git branch --show-current)
-if [[ "$current_branch" = "$BASE_BRANCH" ]]; then
-  echo "cannot run conflict resolution from base branch '$BASE_BRANCH'"
-  exit 1
+echo "git url: $git_url"
+echo "repo: $repo"
+
+if [ "$repo" != "$HEAD_REPO" ]; then
+  echo "Head of PR is from a forked repository -> must add remote"
+  # 2) Add remote
+  echo "Adding remote"
+  forked_url="$git_url/$HEAD_REPO.git"
+  echo "forked url: $forked_url"
+  git remote add forked "$forked_url"
 fi
 
-# git authentication
-git config --global user.name "$USER"
-git config --global user.email "$EMAIL"
+echo "remotes"
+git remote -v
 
-git fetch origin "$BASE_BRANCH"
-git rebase "origin/$BASE_BRANCH"
+# 3) Checkout
 
-# exit if more than poetry is conflicting
-conflict_files=$(git diff --name-only --diff-filter=U --relative)
-if [ "$conflict_files" != "poetry.lock" ]; then
-  echo "conflicts resolution is supported for 'poetry.lock' only"
-  git rebase --abort
-  exit 1
-fi
 
-# keep the local poetry.lock
-git checkout --theirs poetry.lock
-echo "Refreshing poetry.lock"
-poetry lock --no-update
-
-git add poetry.lock
-git -c core.editor=true rebase --continue
-
-echo "Pushing resolved poetry.lock"
-git push -v --force-with-lease origin "$current_branch"
+## install poetry
+#curl -sSL https://install.python-poetry.org | python3 -
+#
+#if ! git ls-remote --heads origin | grep -wq "refs/heads/$BASE_BRANCH"; then
+#  echo "base branch '$BASE_BRANCH' does not exist"
+#  exit 1
+#fi
+#
+#current_branch=$(git branch --show-current)
+#if [[ "$current_branch" = "$BASE_BRANCH" ]]; then
+#  echo "cannot run conflict resolution from base branch '$BASE_BRANCH'"
+#  exit 1
+#fi
+#
+## git authentication
+#git config --global user.name "$USER"
+#git config --global user.email "$EMAIL"
+#
+#git fetch origin "$BASE_BRANCH"
+#git rebase "origin/$BASE_BRANCH"
+#
+## exit if more than poetry is conflicting
+#conflict_files=$(git diff --name-only --diff-filter=U --relative)
+#if [ "$conflict_files" != "poetry.lock" ]; then
+#  echo "conflicts resolution is supported for 'poetry.lock' only"
+#  git rebase --abort
+#  exit 1
+#fi
+#
+## keep the local poetry.lock
+#git checkout --theirs poetry.lock
+#echo "Refreshing poetry.lock"
+#poetry lock --no-update
+#
+#git add poetry.lock
+#git -c core.editor=true rebase --continue
+#
+#echo "Pushing resolved poetry.lock"
+#git push -v --force-with-lease origin "$current_branch"
